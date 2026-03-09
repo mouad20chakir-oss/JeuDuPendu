f9b41590731807d85bd81c281c654b65350a6218
Mouad Chakir <mouad20chakir@gmail.com>
2026-03-09 10:46:43 +0000

    improved UI styling

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const buttons = document.querySelectorAll(.buttons)
+const buttons = document.querySelectorAll(".btn");
 
-password = [];
-password.push()
-for btn of buttons {
-    buttons.addEventListener('click', function)
-}
+words = ["banana","apple","orange","coconut","strawberry"]
+let randomIndex = Math.floor(Math.random()* words.length)
+
+
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function () {
+        for(let i=0;i<words.length;i++){
+            if(button==words[randomIndex]){
+                document.getElementById("feet2").style.visibility = "visible";
+            }
+        }
+        
+        document.getElementsByClassName("input").textContent = button.value;
+    });
+});
